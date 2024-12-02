b0baefff7924db40ed1e6f45ebc2f388e277880f
VrubPetGit <petr@vrubel.online>
2024-12-03 00:06:08 +0100

    In progress of making assessments questions

--- app/data/assessment.py
+++ app/data/assessment.py
@@ -1,13 +1,11 @@
-from app.data.init import curs
-
-
-# -------------------------------
-#   CRUDs
-# -------------------------------
+from app.data.init import conn, curs
+import app.data.question as question_data
+from app.model.assesment import AssessmentNew
+from app.model.question import QuestionCategory
 
 
 curs.execute("""create table if not exists assessments(
-    assessment_id integer primary key,
+    assessment_id text primary key,
     assessment_name text,
     owner_id text references user( user_id ),
     last_edit text,
@@ -16,9 +14,9 @@ curs.execute("""create table if not exists assessments(
 
 
 curs.execute("""create table if not exists assessments_questions(
-    assessment_question_id integer PRIMARY KEY,
-    assessment_id integer references assessments( assessment_id ),
-    assessment_qc_id references assessments_q_categories( assessment_qc_id ),
+    question_id integer PRIMARY KEY,
+    assessment_id text references assessments( assessment_id ),
+    category_id references assessments_questions_categories( category_id ),
     question text,
     question_description text,
     question_order integer,
@@ -27,16 +25,75 @@ curs.execute("""create table if not exists assessments_questions(
     option_no text)""")
 
 
+curs.execute("""create table if not exists assessments_questions_categories(
+    category_id integer primary key,
+    assessment_id text references assessments( assessment_id ),
+    category_name text,
+    category_order integer
+    )""")
+
+
 curs.execute("""create table if not exists assessments_answers(
-    answer_id integer PRIMARY KEY,
-    assessment_id integer references assessments( assessment_id ),
-    assessment_question_id integer references assessments_questions( assessment_question_id ),
+    answer_id text pirmary key,
+    assessment_id text references assessments( assessment_id ),
+    question_id integer references assessments_questions( question_id ),
     answer_option text,
     answer_description text
     )""")
 
-curs.execute("""create table if not exists assessments_q_categories(
-    assessment_qc_id integer primary key,
-    assessment_qc_name text,
-    assessment_qc_order integer
-    )""")
+# -------------------------------
+#   CRUDs
+# -------------------------------
+
+
+def create_assessment(assessment_new: AssessmentNew) -> Assessment:
+
+    # Create new assessment entry
+    qry = """insert into assessments(assessment_id, assessment_name, onwer_id)
+    values(:assessment_id, :assessment_name, :owner_id)"""
+
+    params = {
+            "assessment_id": assessment_new.assessment_id,
+            "assessment_name": assessment_new.assessment_name,
+            "owner_id": assessment_new.onwer_id
+            }
+
+    cursor = conn.cursor()
+
+    try:
+        cursor.execute(qry, params)
+    finally:
+        cursor.close()
+
+
+def freeze_questions_categories(assessment_id: str) -> bool:
+
+    questions_cateogies: list[QuestionCategory] = question_data.get_all_categories()
+
+    for category in questions_cateogies:
+        
+        qry = """insert into
+        assessments_questions_categories(assessment_id, category_name, category_order)
+        values(:assessment_id, :category_name, :category_order)"""
+
+        params = {
+                "assessment_id": assessment_id,
+                "category_name": category.category_name,
+                "category_order": category.category_order
+                }
+
+        cursor = conn.cursor()
+        try:
+            cursor.execute(qry, params)
+        finally:
+            cursor.close()
+    
+    return True
+
+
+def freeze_questions(assessment_id: str) -> bool:
+
+
+    return True
+
+
--- app/model/assesment.py
+++ app/model/assesment.py
@@ -1,8 +1,28 @@
-from typing import Optional
 from pydantic import BaseModel
 
-
 class Assessment(BaseModel):
+    assessment_id: str
 
-    id: Optional[int]
-    owner: str | None
+
+class AssessmentNew(BaseModel):
+    assessment_id: str
+    assessment_name: str
+    owner_id: str
+
+
+class AssessmentQuestion(BaseModel):
+    assessment_id: str
+    assessment_name: str
+    onwer_id: str
+    last_edit: str
+    last_editor: str
+    question_id: int
+    category_id: int
+    category_name: str
+
+
+class AssessmentQuestionCategory(BaseModel):
+    category_id: int
+    assessment_id: str
+    category_name: str
+    category_order: int
